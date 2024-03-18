9e80b38f1ca54644f4aa45190be128bd90e368c4
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-03-18 14:49:59 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins',
+        dynalight: 'Dynalight',
+      },
+      colors: {
+        primary: '#A435F0',
+        secondary: '#fff',
+        tertiary: 'rgb(200,200,200)',
+      },
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+      },
+      animation: {
+        'waving-hand': 'wave 2s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+}
+
